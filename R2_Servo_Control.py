4f5812e6000a67e5fc89c7b789b87b7bea000cc8
dpoulson <daz@22balmoralroad.net>
2013-07-29 00:38:50 +0100

    Update: reading in strings from pipe and servos working

--- R2_Servo_Control.py
+++ R2_Servo_Control.py
@@ -4,8 +4,14 @@ import os
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 import csv
+import collections
 
 servo_config_file = "servo.conf"
+pipePath = "/tmp/r2_commands.pipe"
+
+Servos = collections.namedtuple('Servo', 'address, channel, name, servoMin, servoMax')
+
+servo_list = Servos(address='0x40', channel = 7, name = "PP2", servoMin = 140, servoMax = 660)
 
 def init():
    # Load in CSV of Servo definitions
@@ -13,15 +19,27 @@ def init():
    reader = csv.reader(ifile)
    
 
-pipePath = "/tmp/r2_commands.pipe"
+
+pwm = PWM(0x40, debug=False)
+pwm.setPWMFreq(60) 
+
+def servo_command(servo_name, position):
+   actual_position = int(((servo_list.servoMax - servo_list.servoMin)*float(position)) + servo_list.servoMin)
+   print "Setting servo %s to position = %s" % (servo_name, actual_position)
+   pwm.setPWM(servo_list.channel, 0, actual_position)
+
+
+
+# Main loop
 
 rp = open(pipePath, 'r')
 while(True):
    response = rp.readline()
-   command = response.strip
+   command = response.rstrip('\r\n ')
    if command != "":
-      print "Got %s" % response
-      response=""
+      command_name = command.split(',')[0]
+      command_position = command.split(',')[1]
+      servo_command(command_name, command_position)
 
 rp.close()
 
