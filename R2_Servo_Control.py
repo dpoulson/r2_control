ef8cc45db77b6bb843edff025db20948fb71b298
dpoulson <daz@22balmoralroad.net>
2013-07-29 01:42:32 +0100

    Reloads config with command RELOAD,0

--- R2_Servo_Control.py
+++ R2_Servo_Control.py
@@ -11,26 +11,46 @@ pipePath = "/tmp/r2_commands.pipe"
 
 Servos = collections.namedtuple('Servo', 'address, channel, name, servoMin, servoMax')
 
-servo_list = Servos(address='0x40', channel = 7, name = "PP2", servoMin = 140, servoMax = 660)
+servo_list = []
 
-def init():
+
+def init_config():
    # Load in CSV of Servo definitions
    ifile = open(servo_config_file, "rb")
    reader = csv.reader(ifile)
-   
+   for row in reader:
+      servo_address = row[0]
+      servo_channel = int(row[1])
+      servo_name = row[2]
+      servo_servoMin = int(row[3])
+      servo_servoMax = int(row[4])
+      servo_list.append(Servos(address=servo_address, channel = servo_channel, name = servo_name, servoMin = servo_servoMin, servoMax = servo_servoMax))
+   print servo_list
 
 
-pwm = PWM(0x40, debug=False)
-pwm.setPWMFreq(60) 
+
 
 def servo_command(servo_name, position):
-   actual_position = int(((servo_list.servoMax - servo_list.servoMin)*float(position)) + servo_list.servoMin)
-   print "Setting servo %s to position = %s" % (servo_name, actual_position)
-   pwm.setPWM(servo_list.channel, 0, actual_position)
+   current_servo = []
+   for servo in servo_list:
+      if servo.name == servo_name:
+         current_servo = servo
+   if float(position) > 1 or float(position) < 0 or not current_servo:
+      print "Invalid name or position (%s, %s)" % (servo_name, position)
+   else: 
+      actual_position = int(((current_servo.servoMax - current_servo.servoMin)*float(position)) + current_servo.servoMin)
+      print "Setting servo %s(%s) to position = %s(%s)" % (servo_name, current_servo.channel, actual_position, position)
+      pwm.setPWM(current_servo.channel, 0, actual_position)
 
 
 
 # Main loop
+
+init_config()
+
+# Initialise servo controllers
+pwm = PWM(0x40, debug=False)
+pwm.setPWMFreq(60)  
 
 rp = open(pipePath, 'r')
 while(True):
@@ -39,7 +59,12 @@ while(True):
    if command != "":
       command_name = command.split(',')[0]
       command_position = command.split(',')[1]
-      servo_command(command_name, command_position)
+      if command_name == "RELOAD":
+         print "Reloading config file"
+         servo_list = []
+         init_config()
+      else:
+         servo_command(command_name, command_position)
 
 rp.close()
 
