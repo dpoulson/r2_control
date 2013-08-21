fdf917b0961425a2692c4e3de46d07539ebd2e25
dpoulson <daz@22balmoralroad.net>
2013-08-21 23:11:38 +0100

    Added initial threading Added home sub routine to reset servo positions Tidied code a little Added inital code for speed options

--- R2_Servo_Control.py
+++ R2_Servo_Control.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
+#===============================================================================
+# Copyright (C) 2013 Darren Poulson
+#
+# This file is part of R2_Control.
+#
+# pyfa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyfa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyfa.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
 
 import os
+import thread
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 import csv
@@ -10,7 +29,7 @@ servo_config_file = "servo.conf"
 pipePath = "/tmp/r2_commands.pipe"
 servo_list = []
 
-Servos = collections.namedtuple('Servo', 'address, channel, name, servoMin, servoMax')
+Servos = collections.namedtuple('Servo', 'address, channel, name, servoMin, servoMax, servoHome')
 
 def init_config():
    # Load in CSV of Servo definitions
@@ -22,30 +41,44 @@ def init_config():
       servo_name = row[2]
       servo_servoMin = int(row[3])
       servo_servoMax = int(row[4])
-      servo_list.append(Servos(address=servo_address, channel = servo_channel, name = servo_name, servoMin = servo_servoMin, servoMax = servo_servoMax))
+      servo_home = int(row[5])
+      servo_list.append(Servos(address = servo_address, channel = servo_channel, name = servo_name, servoMin = servo_servoMin, servoMax = servo_servoMax, servoHome = servo_home))
    ifile.close()
 
-def servo_command(servo_name, position):
+# Send a command over i2c to turn a servo to a given position (percentage) over a set duration (seconds)
+def servo_command(servo_name, position, duration):
    current_servo = []
+   try:
+      position = float(position)
+   except:
+      print "Position not a float"
    for servo in servo_list:
       if servo.name == servo_name:
          current_servo = servo
-   if float(position) > 1 or float(position) < 0 or not current_servo:
+   if position > 1 or position < 0 or not current_servo:
       print "Invalid name or position (%s, %s)" % (servo_name, position)
    else: 
-      actual_position = int(((current_servo.servoMax - current_servo.servoMin)*float(position)) + current_servo.servoMin)
-      print "Setting servo %s(%s) to position = %s(%s)" % (servo_name, current_servo.channel, actual_position, position)
+      actual_position = int(((current_servo.servoMax - current_servo.servoMin)*position) + current_servo.servoMin)
+      print "Setting servo %s(%s) to position = %s(%s) with duration = %s" % (servo_name, current_servo.channel, actual_position, position, duration)
       pwm.setPWM(current_servo.channel, 0, actual_position)
 
-# Main loop
-
-init_config()
+# Set all servos to home position
+def servo_home():
+   for servo in servo_list:
+      pwm.setPWM(servo.channel, 0, servo.servoHome)
 
 # Initialise servo controllers
 pwm = PWM(0x40, debug=False)
 pwm.setPWMFreq(60)  
 
+# Main loop
+
+init_config()
+servo_home()
+
 rp = open(pipePath, 'r')
+
+# Listen for commands on the pipe
 while(True):
    response = rp.readline()
    command = response.rstrip('\r\n ')
@@ -55,13 +88,23 @@ while(True):
          print "Reloading config file"
          servo_list = []
          init_config()
+         servo_home()
       elif command == "QUIT":
          print "Quitting..."
          break
+      elif command == "HOME":
+         servo_home()
       else: 
          command_name = command.split(',')[0]
          command_position = command.split(',')[1]
-         servo_command(command_name, command_position)
+         try:
+            command_duration = command.split(',')[2]
+         except:
+            command_duration = 0
+	 try:
+            thread.start_new_thread( servo_command, (command_name, command_position, command_duration) )
+         except:
+            print "Error: unable to start thread"
 
 rp.close()
 
