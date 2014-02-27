831adb6a37300c821b2f2aea3beb70ce2cb41940
dpoulson <daz@22balmoralroad.net>
2014-02-27 00:17:25 +0000

    Updated a few files and added web site

--- R2_Servo_Control.py
+++ R2_Servo_Control.py
@@ -4,18 +4,18 @@
 #
 # This file is part of R2_Control.
 #
-# pyfa is free software: you can redistribute it and/or modify
+# R2_Control is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# pyfa is distributed in the hope that it will be useful,
+# R2_Control is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pyfa.  If not, see <http://www.gnu.org/licenses/>.
+# along with R2_Control.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
 import os, sys
