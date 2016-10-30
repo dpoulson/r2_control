5991f10642547824abbae9654a2c0a41ad2a15d5
Darren Poulson <darren.poulson@gmail.com>
2016-10-30 14:56:52 +0000

    Some tweaks to dome servos, and updates to ps3 controller

--- controllers/ps3/r2_ps3.py
+++ controllers/ps3/r2_ps3.py
@@ -1,15 +1,17 @@
 import os
+import sys
 import pygame
 import time
 import random
 import csv
 import requests
+from collections import defaultdict
 from cStringIO import StringIO
 
 baseurl = "http://localhost:5000/"
  
-if not os.getenv('SDL_VIDEODRIVER'):
-    os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+
 
 pygame.display.init()
 pygame.init()
@@ -25,17 +27,22 @@ j.init()
 buttons = j.get_numbuttons()
 
 # Read in key combos from csv file
-keys = {}
+keys = defaultdict(list)
 with open('keys.csv', mode='r') as infile:
    reader = csv.reader(infile)
    for row in reader:
       print "Row: %s | %s | %s" % (row[0], row[1], row[2])
-      #keys += {row[0]:row[1]}
-      keys[row[0]][0] = row[1]
-      keys[row[0]][1] = row[2]
+      keys[row[0]].append(row[1])
+      keys[row[0]].append(row[2])
+#      keys.update({row[0]:row[1]})
+#      keys[row[0]][0] = row[1]
+#      keys[row[0]][1] = row[2]
+
+keys.items()
 
 # Main loop
 while True:
+   global previous
    events = pygame.event.get()
    for event in events:
       if event.type == pygame.JOYBUTTONDOWN:
@@ -44,18 +51,27 @@ while True:
             button = j.get_button(i)
             buf.write(str(button))
          combo = buf.getvalue()
-         print "Buttons: %s" % combo
+         print "Buttons pressed: %s" % combo
          try:
+            print "Would run: %s" % keys[combo]
             newurl = baseurl + keys[combo][0]
             print "URL: %s" % newurl
             r = requests.get(newurl)
-            if keys[combo][1] != "":
-               print "Running on button release option"
-               newurl = baseurl + keys[combo][0]
-               print "URL: %s" % newurl
-               r = requests.get(newurl)
+            print "Command done"
          except:
-            print "No combo"
+            print "No combo (pressed)"
+         previous = combo 
+      if event.type == pygame.JOYBUTTONUP:
+         print "Buttons released: %s" % previous
+         try:
+            print "Would run: %s" % keys[previous][1]
+            newurl = baseurl + keys[previous][1]
+            print "URL: %s" % newurl
+            r = requests.get(newurl)
+            print "Command done"
+         except:
+            print "No combo (released)"
+
 
 
 
