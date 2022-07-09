eaa845b696c44e8aa8433707418a4268468e9c90
Darren Poulson <darren.poulson@gmail.com>
2022-07-09 14:21:31 +0100

    Fixed some typos in the joystick wrapper

--- controllers/joystick_wrapper.sh
+++ controllers/joystick_wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CONT_DIR=/home/pi/.r2config
+CONT_DIR=/home/pi/.r2_config
 
 CURRENT=`cat $CONT_DIR/current_joy`
 rm $CONT_DIR/.shutdown
 
 echo "Joystick selected: $CURRENT"
 
-cd /home/pi/r2_control/$CURRENT
+cd /home/pi/r2_control/controllers/$CURRENT
 
 /usr/bin/python -O ./r2_$CURRENT.py
 
